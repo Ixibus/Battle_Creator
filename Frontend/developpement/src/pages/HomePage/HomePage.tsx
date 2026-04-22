629de930d7ed0dad2d695bec0f4435c7003bde3a
Franck CORCHER <corcher.franck@gmail.com>
2026-04-22 12:29:58 +0200

    create: home Page created

--- Frontend/developpement/src/pages/HomePage/HomePage.tsx
+++ Frontend/developpement/src/pages/HomePage/HomePage.tsx
@@ -5,16 +5,45 @@ export default function HomePage() {
     <>
       <div className="homePageDisplayer">
         <div className="topLeftContainer">
-          <div>Membres + Taches</div>
+          <div className="membersContainer">Membres</div>
+          <div className="tasksContainer">Tâches</div>
         </div>
         <div className="topRightContainer">
-          <div>Progression + Materiels</div>
+          <div className="propsContainer">Materiels</div>
+          <div className="progressionContainer">76%</div>
         </div>
         <div className="mandatoryMissionsContainer">
-          <div>Missions obligatoires</div>
+          <div className="missionsContainer mandatory">Espace</div>
+          <div className="missionsContainer mandatory">MC</div>
+          <div className="missionsContainer mandatory">Ticketterie</div>
+          <div className="missionsContainer mandatory">Juges</div>
+          <div className="missionsContainer mandatory">Dj</div>
+          <div className="missionsContainer mandatory">Planning</div>
+          <div className="missionsContainer mandatory">Système son</div>
         </div>
         <div className="optionnalMissionsContainer">
-          <div>Missions optionnelles</div>
+          <div className="missionsContainer">Communication évènement</div>
+          <div className="missionsContainer">Vestiaires</div>
+          <div className="missionsContainer">Matériels optionnelles d'ambiance</div>
+          <div className="missionsContainer">Objets signalétiques</div>
+          <div className="missionsContainer">Matériels Participants</div>
+          <div className="missionsContainer">Personnel d'encadrement</div>
+          <div className="missionsContainer">Ravitaillement Juge</div>
+          <div className="missionsContainer">Mission 1</div>
+          <div className="missionsContainer">Mission 2</div>
+          <div className="missionsContainer">Mission 3</div>
+          <div className="missionsContainer">Mission 4</div>
+          <div className="missionsContainer">Mission 5</div>
+          <div className="missionsContainer">Mission 6</div>
+          <div className="missionsContainer">Mission 7</div>
+          <div className="missionsContainer">Mission 8</div>
+          <div className="missionsContainer">Mission 9</div>
+          <div className="missionsContainer">Mission 10</div>
+          <div className="missionsContainer">Mission 11</div>
+          <div className="missionsContainer">Mission 12</div>
+          <div className="missionsContainer">Mission 13</div>
+          <div className="missionsContainer">Mission 14</div>
+          <div className="missionsContainer">Mission 15</div>
         </div>
       </div>
     </>
