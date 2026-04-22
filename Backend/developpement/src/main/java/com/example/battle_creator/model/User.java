b9a4860cd876349b84787dd9f8bbacf456a3885f
Franck CORCHER <corcher.franck@gmail.com>
2026-04-22 18:02:36 +0200

    update: User model

--- Backend/developpement/src/main/java/com/example/battle_creator/model/User.java
+++ Backend/developpement/src/main/java/com/example/battle_creator/model/User.java
@@ -1,4 +1,25 @@
 public class User{
 
-    private 
+    private int id;
+
+    private string email;
+
+    private string login;
+
+    private Boolean isActive;
+
+    private Long createdAt;
+
+    private Long updatedAt;
+
+    public User(){};
+
+    public User(int id, string email, string login, Boolean isActive, Long createdAt, Long updatedAt) {
+        this.id = id;
+        this.email = email;
+        this.login = login;
+        this.isActive = isActive;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
