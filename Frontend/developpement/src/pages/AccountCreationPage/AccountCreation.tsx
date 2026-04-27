2a26eb8eb60a449223a57157e8fd9f189bb2533a
Franck CORCHER <corcher.franck@gmail.com>
2026-04-27 18:38:56 +0200

    update - ProjectController.java: adding CORS acceptation for CRUD; update - AccountCreation.tsx: POST method with fetch is functionnal

--- Frontend/developpement/src/pages/AccountCreationPage/AccountCreation.tsx
+++ Frontend/developpement/src/pages/AccountCreationPage/AccountCreation.tsx
@@ -3,14 +3,14 @@ import InputContainer from "../../components/InputContainer/InputContainer";
 import { useState } from "react";
 import LoginButton from "./LoginButton";
 
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function AccountCreation() {
   const { isAuthenticated, isLoading, error } = useAuth0();
 
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [pseudo, setPseudo] = useState("");
+  const [login, setLogin] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -20,23 +20,45 @@ export default function AccountCreation() {
   // }
 
   const form = document.querySelector("form");
-  function handlesubmitTester(e : any, email : string, pseudo: string) {
+  async function handlesubmitTester(e: any) {
     e.preventDefault();
     // test.append("Le pseudo", pseudo);
     // test.append("Le mail", email);
     // console.log(test.getAll("Le pseudo"), test.getAll("Le mail"));
-    
+
     // Avec "form!"" par défaut dans le paramètre, l'objet FormData sera rempli en clé/valeur avec :
     // la clé = la valeur de la propriété "name" de l'input enfant de <form>
     // la valeur = la valeur de la propriété "valeur" de cette input enfant susvisé
     const inputDatas = new FormData(form!);
     for (const el of inputDatas.entries()) {
       // el[0] = la clé; el[1] = sa valeur
-      console.log(el[0] + ", "+ el[1]);
+      console.log(el[0] + ": " + el[1]);
     }
 
-    // console.log(test.entries());
+    const dataResponse = await fetch("http://localhost:8080/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        login: login,
+        isActive: false,
+      }),
+    });
 
+    let result = await dataResponse.json();
+
+    console.log(dataResponse);
+    console.log(result);
+    // const dataResponse = await fetch("http://localhost:8080/projects");
+
+    //   let result = await dataResponse.json();
+
+    //   console.log(dataResponse);
+    //   console.log(result);
+
+    // console.log(test.entries());
   }
 
   return (
@@ -50,15 +72,25 @@ export default function AccountCreation() {
         <InputContainer labelName="Confirmer votre mot de passe" htmlFor="passwordConfirmation" type="password"/>
         <button type="submit">valider</button>
       </form>} */}
-      {!isAuthenticated && <form className="form" onSubmit={(e) => handlesubmitTester(e, email, pseudo)}>
-        <h1>TEST CREATION DE COMPTE</h1>
-        <InputContainer labelName="Votre email" htmlFor="email" type="email" onChange={(e) => setEmail(e.target.value)}/>
-        <InputContainer labelName="Votre pseudo" htmlFor="pseudo" type="text" onChange={(e) => setPseudo(e.target.value)}/>
-                <button type="submit">valider</button>
-      </form>}
-      {!isAuthenticated &&<LoginButton/>}
+      {!isAuthenticated && (
+        <form className="form" onSubmit={(e) => handlesubmitTester(e)}>
+          <h1>TEST CREATION DE COMPTE</h1>
+          <InputContainer
+            labelName="Votre email"
+            htmlFor="email"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <InputContainer
+            labelName="Votre login"
+            htmlFor="login"
+            type="text"
+            onChange={(e) => setLogin(e.target.value)}
+          />
+          <button type="submit">valider</button>
+        </form>
+      )}
+      {!isAuthenticated && <LoginButton />}
     </>
   );
 }
-
-
