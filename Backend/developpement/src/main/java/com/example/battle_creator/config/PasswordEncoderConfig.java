2044f63d94d968955c38c53098bd7f77be4d4abf
Franck CORCHER <corcher.franck@gmail.com>
2026-05-08 11:54:05 +0200

    fix - PasswordEncoderConfig : debugging argon2 problem encoder with 'main args'; adding bouncyCastle in pom.xml

--- Backend/developpement/src/main/java/com/example/battle_creator/config/PasswordEncoderConfig.java
+++ Backend/developpement/src/main/java/com/example/battle_creator/config/PasswordEncoderConfig.java
@@ -1,16 +1,28 @@
 package com.example.battle_creator.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+//@Configuration
 public class PasswordEncoderConfig {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return  Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+//    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
     }
+
+    final static String mdp = "monMotDePasse";
+
+    static String mdpHash = passwordEncoder().encode(mdp);
+
+    static String test = "hello";
+
+
+    public static void main(String[] args) {
+        System.out.println(mdpHash);
+//        System.out.println(test);
+    }
+
 }
 
