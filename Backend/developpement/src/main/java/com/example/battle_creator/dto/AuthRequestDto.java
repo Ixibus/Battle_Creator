2ad70fa3ca3e047b17c12f1d93ce27760810c9eb
Franck CORCHER <corcher.franck@gmail.com>
2026-05-11 14:52:54 +0200

    update - AuthRequestDto.java

--- Backend/developpement/src/main/java/com/example/battle_creator/dto/AuthRequestDto.java
+++ Backend/developpement/src/main/java/com/example/battle_creator/dto/AuthRequestDto.java
@@ -1,7 +1,12 @@
 package com.example.battle_creator.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class AuthRequestDto {
+
     private String login;
+
+    @JsonProperty("password")
     private String rawPassword;
 
     public String getLogin() { return login; }
