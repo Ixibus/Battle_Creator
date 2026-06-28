679148b69125b5eac9089087b1ccae0536295fa1
Franck CORCHER <corcher.franck@gmail.com>
2026-06-28 15:44:17 +0200

    update - GlobalExceptionHandler.java: udpdate 409 error message

--- Backend/developpement/src/main/java/com/example/battle_creator/exception/GlobalExceptionHandler.java
+++ Backend/developpement/src/main/java/com/example/battle_creator/exception/GlobalExceptionHandler.java
@@ -15,6 +15,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(org.springframework.dao.DataIntegrityViolationException.class)
     public ResponseEntity<String> handleDataIntegrity(org.springframework.dao.DataIntegrityViolationException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body("Au niveau de la base de données, la ou les valeur(s) est/sont déjà  présente(s) (CREATE?), n'existe(nt) pas (READ?, UPDATE?, DELETE?) ou n'est/ne sont pas conforme(s) (CREATE?)");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Erreur 409 (intégrité donnée en BDD et/ou durant la navette dto->service->model) : Au niveau de la base de données, la ou les valeur(s) est/sont déjà  présente(s) (CREATE?), n'existe(nt) pas (READ?, UPDATE?, DELETE?) ou n'est/ne sont pas conforme(s) (CREATE?)");
     }
 }
